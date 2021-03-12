ab5469d6d80a6f5ed908a01069dc8cc73a0ff626
ecourse <smkang@codenuri.co.kr>
2021-03-12 16:45:01 +0900

    Add files via upload

---
+++ 3_함수객체3.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <bitset>   // STL 의 있는 "비트"를 관리하는 클래스
+
+// 함수객체는 상태(멤버데이타)를 가질수 있다.!!!
+
+class URandom
+{
+	// 멤버 데이타(상태보관)
+	std::bitset<10> bs;
+	bool recycle;
+public:
+	URandom(bool b = false) : recycle(b)
+	{
+		//bs.set(3); // 3번째 비트를 1로
+		bs.set();  // 모든 비트를 1로
+	}
+	   
+
+	int operator()()
+	{
+		if (bs.none()) // 모두 0
+		{
+			if (recycle == true)
+				bs.set(); // 다시 모두 1로
+			else
+				return -1;
+		}
+
+		int k = -1;
+
+		while ( !bs.test(k = rand() % 10) );
+
+		bs.reset(k);
+
+		return k;
+	}
+
+
+
+
+
+};
+
+int main()
+{
+	//URandom urand;
+	URandom urand(true);
+
+	for (int i = 0; i < 15; i++)
+		std::cout << urand() << std::endl;
+}
+
+
+
+
+
+
