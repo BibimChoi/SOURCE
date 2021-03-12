76e981bc3c2d1d0d1cb09b2cac4fb7e9d07025fa
ecourse <smkang@codenuri.co.kr>
2021-03-12 11:18:58 +0900

    Add files via upload

---
+++ 2_람다표현식1.cpp
@@ -0,0 +1,19 @@
+﻿// 2_람다표현식1 - 127 page
+
+int add(int a, int b) { return a + b; }
+
+void foo(  int(*f)(int, int) )
+{
+}
+
+int main()
+{
+	foo(add); // ok 
+
+	// 람다 표현식 : 함수이름(또는 주소)가 필요한 자리에 
+	//				함수 구현 자체를 표기하는 문법
+	// [] :람다가 시작됨을 알리는 기호
+	//		"lambda introducer" 라고 합니다.
+
+	foo( [](int a, int b) { return a + b; } );
+}
---
+++ 2_람다표현식2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool foo(int n) { return n % 3 == 0;  }
+
+int main()
+{
+	std::vector<int> v = { 1,2,6,5,4,3,7,8 };
+
+	// v에서 3을 찾고 싶다.
+	auto p1 = std::find(begin(v), end(v), 3);  // "값 검색"
+	std::cout << *p1 << std::endl; // 3
+
+	// v에서 1번째 나오는 3의 배수를 찾고 싶다.? => "조건 검색"
+
+	//auto p2 = std::find_if(begin(v), end(v), foo ); // C++98 스타일
+
+	// C++11 : 함수 이름(주소)을 전달해야 하는 곳에 람다 표현식을 사용하자!
+	auto p2 = std::find_if(begin(v), end(v), [](int n) { return n % 3 == 0;}); // C++11 스타일
+	
+	std::cout << *p2 << std::endl; // 6
+}
+
+
+
+
