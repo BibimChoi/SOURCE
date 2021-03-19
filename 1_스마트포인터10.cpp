faece411645dbb63b284bd801b0515661ef19e0c
ecourse <smkang@codenuri.co.kr>
2021-03-19 11:10:07 +0900

    Add files via upload

--- 1_스마트포인터10.cpp
+++ 1_스마트포인터10.cpp
@@ -17,7 +17,6 @@ int main()
 	
 		//sp = p;  // 참조계수 2.
 		wp = p;		// 참조계수 증가 안함. 계속 1.
-		
 		std::cout << p.use_count() << std::endl; // 1
 //	} // p 파괴.. 즉 객체도 파괴
 
@@ -29,6 +28,13 @@ int main()
 	{
 		// wp로 객체 사용
 		wp->Go(); // error.. 그런데.. 안된다!!!
+
+		// wp로 객체에 접근하려면 "다시 shared_ptr을 만들어서 참조계수를 증가한후"
+		// 접근해야 한다.
+		std::shared_ptr<Car> sp2 = wp.lock();
+
+		if (sp2) // 안전한지 확인하고
+			sp2->Go(); // 사용해야 한다.
 	}
 
 	std::cout << "------" << std::endl;
