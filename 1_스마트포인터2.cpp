e4de389198f57ff4e142e64b4fe6556cf1ee720d
ecourse <smkang@codenuri.co.kr>
2021-03-19 13:20:41 +0900

    Add files via upload

---
+++ 1_스마트포인터2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+// 1. template 으로 만들어야 한다.
+// 2. 얕은 복사 문제를 해결해야 한다.
+//    (A) 깊은 복사
+//    (B) 참조 계수
+//    (C) 복사 금지
+//    (D) move 개념
+
+template<typename T> class Ptr
+{
+	T* obj;
+public:
+	Ptr(T* p = nullptr) : obj(p) {}
+
+	~Ptr() { delete obj; }
+
+	T* operator->() { return obj; }
+	T& operator*() { return *obj; }
+};
+int main()
+{
+	Ptr<int> p1 = new int;
+	*p1 = 10;
+	std::cout << *p1 << std::endl;
+
+	Ptr<int> p2 = p1; // 얕은 복사 발생
+}
+
+
