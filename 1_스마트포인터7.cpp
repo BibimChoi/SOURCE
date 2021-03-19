7ea68252cbb57600f86b9693c5dab710bdcedfe5
ecourse <smkang@codenuri.co.kr>
2021-03-19 09:27:48 +0900

    Add files via upload

---
+++ 1_스마트포인터7.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <memory>
+
+struct People
+{
+	std::string name;
+	People(std::string n) : name(n) {}
+	~People() { std::cout << name << " 파괴" << std::endl; }
+
+	std::shared_ptr<People> bf; // best friend
+};
+
+int main()
+{
+	std::shared_ptr<People> sp1(new People("kim"));
+	std::shared_ptr<People> sp2(new People("lee"));
+
+	// shared_ptr 사용시 "상호참조", "순환참조(cycle reference)"
+	// 발생시 자원 누수가 있습니다.
+	sp1->bf = sp2;
+	sp2->bf = sp1;
+}
+
+
