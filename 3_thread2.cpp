bb2b38802508800ec72531ca4f21755e8dfc4965
ecourse <smkang@codenuri.co.kr>
2021-03-19 15:33:35 +0900

    Add files via upload

---
+++ 3_thread2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <thread> 
+
+void foo(int n, double d)
+{
+	std::cout << "foo" << std::endl;
+}
+
+class Test
+{
+public:
+	void goo(int n, double d)
+	{
+		std::cout << "goo" << std::endl;
+	}
+};
+
+int main()
+{
+	std::thread t1(&foo, 1, 3.4); // 이순간 스레드 생성
+
+	t1.join();	// pthread_join();
+//	t1.detach();// 새로운 스레드를 대기하지 않겠다는 것.  
+
+	Test test;
+	std::thread t2(&Test::goo, &test, 1, 3.4);
+	t2.join();
+}
+
+
+
+
+
---
+++ 3_thread3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex m;
+int shared_data = 10;
+
+// 동기화 자원을 직접 사용하면 예외 발생시 dead lock 의 확률이 있다.
+/*
+void foo()
+{
+	m.lock();
+	// 공유 자원 사용	
+	shared_data = 200;
+
+	m.unlock();
+}
+*/
+void foo()
+{
+	std::lock_guard<std::mutex> lg(m); // 생성자에서 m.lock()
+									   // 소멸자에서 m.unlock()
+	shared_data = 200;
+}
+
+
+
+int main()
+{
+	std::thread t1(&foo);
+	std::thread t2(&foo);
+
+	t1.join();
+	t2.join();
+}
+
