9b73504dc6b86b3e2a5bae5eb06eb2d39a3dae76
ecourse <smkang@codenuri.co.kr>
2021-03-22 13:26:01 +0900

    Add files via upload

--- 3_constructor2.cpp
+++ 3_constructor2.cpp
@@ -20,12 +20,28 @@ public:
 };
 //------------------------------------------
 // 버퍼를 가진 PipeStream 설계
+/*
 class PipeStream : public Stream
 {
-	Buffer buff{ 1024 }; 
+	Buffer buff{ 1024 };  // C++11 부터 가능한 초기화
 public:
 	PipeStream() : Stream(buff) {}
 };
+*/
+
+// 컴파일러가 위 코드를 아래 처럼 변경합니다.
+class PipeStream : public Stream
+{
+	Buffer buff;
+public:
+//	PipeStream() : Stream(buff), buff{ 1024 } {} // 컴파일러가 이렇게 변경
+	PipeStream() : buff(1024), Stream(buff) {} // 이렇게 코딩해도.. 실제 호출은 
+												// 기반 클래스 생성자 먼저 호출
+	// 결론 : 멤버 데이타는 기반 클래스 생성자에 보낼때 주의 하세요
+	//		  초기화되지 않은 데이타를 전달하고 있는 것 입니다.
+};
+
+
 
 int main()
 {
