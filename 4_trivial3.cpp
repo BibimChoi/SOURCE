829b9e76768b5273ea4e6fdcc7412b6370561ea7
ecourse <smkang@codenuri.co.kr>
2021-03-22 14:27:06 +0900

    Add files via upload

---
+++ 4_trivial3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+struct Point
+{
+	int x, y;
+
+	Point() = default;
+	Point(int a, int b) : x(a), y(b) {}
+};
+
+// 왜 trivial 여부를 조사하는가 ?
+template<typename T> 
+void copy(T* dst, T* src, std::size_t sz)
+{
+	if ( std::is_trivially_copyable_v<T> )
+	{
+		std::cout << "버퍼를 통째로 복사" << std::endl;
+		memcpy(dst, src, sizeof(T)*sz);
+	}
+	else
+	{
+		std::cout << "요소를 한개씩 복사 생성자로 복사" << std::endl;
+		while (sz--)
+		{
+			new(dst) T(*src); // 복사 생성자 호출 모양 - "placement new 수업"
+			++dst, ++src;
+		}
+	}
+}
+
+int main()
+{
+	Point pt1[5] = { {1,1}, {2,2}, {3,3}, {4,4}, {5,5} };
+	Point pt2[5];
+
+	copy(pt2, pt1, 5);
+
+}
