62e3f3cb069c7180ae65f9140f0c1e7da4fdb6d7
ecourse <smkang@codenuri.co.kr>
2021-03-24 08:49:32 +0900

    Add files via upload

--- 0_임시객체5.cpp
+++ 0_임시객체5.cpp
@@ -3,6 +3,10 @@
 struct Base
 {
 	int value = 10; 
+
+	Base()  { std::cout << "Base()" << std::endl; }
+	Base(const Base& b) : value(200)  { std::cout << "Base()" << std::endl; }
+	~Base() { std::cout << "~Base()" << std::endl; }
 };
 struct Derived : public Base
 {
@@ -14,11 +18,13 @@ int main()
 	std::cout << d.value << std::endl;  // 20
 	std::cout << d.Base::value << std::endl;  // 10
 
+	std::cout << "-------------" << std::endl;
 	std::cout << static_cast<Base>(d).value << std::endl;  // 10
+	std::cout << "-------------" << std::endl;
 	std::cout << static_cast<Base&>(d).value << std::endl;  // 10
 
-	static_cast<Base>(d).value = 20;  // error
-	static_cast<Base&>(d).value = 20; // ok
+//	static_cast<Base>(d).value = 20;  // error
+//	static_cast<Base&>(d).value = 20; // ok
 
 }
 
