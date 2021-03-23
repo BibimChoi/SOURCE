c5cb262b457435632e3c40e70268422f0eb14b2f
ecourse <smkang@codenuri.co.kr>
2021-03-23 15:42:50 +0900

    Add files via upload

--- 10_concept1.cpp
+++ 10_concept1.cpp
@@ -1,10 +1,24 @@
 // 10_concept1
 #include <iostream>
 #include <type_traits>
+#include <concepts>
 
-// template : 모든 타입
+// template : 모든 타입 함수 생성
+// concept 문법 : template 사용시 조건을 만족할때만 사용하겠다.
 
-template<typename T> void foo(T a)
+// 타입이 가져야 하는 조건을 명시하는 문법
+template<typename T>
+concept Addable = requires(T a, T b)
+{
+	{a + b};
+	{a - b};
+//	typename T::type;
+//	a.foo();
+};
+
+//template<typename T> requires std::is_integral_v<T>
+template<typename T> requires Addable<T>
+void foo(T a)
 {
 }
 
