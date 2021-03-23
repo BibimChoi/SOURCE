23870ab153f01812bad50a2c02874d376f90134d
ecourse <smkang@codenuri.co.kr>
2021-03-24 08:43:54 +0900

    Add files via upload

---
+++ 0_임시객체5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+struct Base
+{
+	int value = 10; 
+};
+struct Derived : public Base
+{
+	int value = 20;
+};
+int main()
+{
+	Derived d;
+	std::cout << d.value << std::endl;  // 20
+	std::cout << d.Base::value << std::endl;  // 10
+
+	std::cout << static_cast<Base>(d).value << std::endl;  // 10
+	std::cout << static_cast<Base&>(d).value << std::endl;  // 10
+
+	static_cast<Base>(d).value = 20;  // error
+	static_cast<Base&>(d).value = 20; // ok
+
+}
+
