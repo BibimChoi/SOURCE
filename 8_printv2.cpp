80442b619039d15933abd8adbc7e5f0de2615c31
ecourse <smkang@codenuri.co.kr>
2021-03-23 14:13:40 +0900

    Add files via upload

--- 8_printv2.cpp
+++ 8_printv2.cpp
@@ -3,17 +3,17 @@
 
 // 방법 2. true_type/false_type을 사용한 오버로딩
 
-template<typename T> void printv_imp(const T& v, true_type)
+template<typename T> void printv_imp(const T& v, std::true_type)
 {
 	std::cout << v << " : " << *v << std::endl;
 }
-template<typename T> void printv_imp(const T& v, false_type)
+template<typename T> void printv_imp(const T& v, std::false_type)
 {
 	std::cout << v << std::endl;
 }
 template<typename T> void printv(const T& v)
 {
-	printv_imp(v, is_pointer<T>()); // is_pointer 의 객체 생성
+	printv_imp(v, std::is_pointer<T>()); // is_pointer 의 객체 생성
 }
 int main()
 {
