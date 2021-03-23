3c38df8b982744c84eb061e0414d2afbe65f34c9
ecourse <smkang@codenuri.co.kr>
2021-03-23 13:38:19 +0900

    Add files via upload

---
+++ 7_enable_if3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <type_traits>
+
+template<typename T>
+typename std::enable_if< std::is_integral_v<T>, T>::type 
+gcd(T a, T b)
+{
+	//static_assert(std::is_integral_v<T>, "error, T is no integer");
+
+	return 0;
+}
+
+double gcd(double, double) { std::cout << "double" << std::endl; return 0; }
+
+int main()
+{
+	gcd(6, 9);
+
+	gcd(3.1, 5.4);
+	gcd(3.1f, 5.4f); 
+}
