c31c5104be85cdb38d02a2ef4ed5719287c86203
ecourse <smkang@codenuri.co.kr>
2021-03-23 09:38:05 +0900

    Add files via upload

--- 4_type_traits3.cpp
+++ 4_type_traits3.cpp
@@ -1,12 +1,28 @@
 ﻿#include <iostream>
-template<typename T> struct is_pointer
+
+
+// int a    => 변수이름 : a   a의 타입 : int
+// int x[3] => 변수이름 : x   x의 타입 : int[3]
+//										 T[N] : 크기를 알수 있는 배열
+//										 T[]  : array of unknown bound
+//												(C 언어 flexible array 에서 사용)
+
+template<typename T> struct is_array
 {
 	static constexpr bool value = false;
 };
-template<typename T> struct is_pointer<T*>
+
+template<typename T> struct is_array<T[]>
 {
 	static constexpr bool value = true;
 };
+
+template<typename T, int N> struct is_array<T[N]>
+{
+	static constexpr bool value = true;
+};
+
+
 
 template<typename T> void foo(const T& a)
 {
@@ -15,6 +31,7 @@ template<typename T> void foo(const T& a)
 	else
 		std::cout << "배열 아님" << std::endl;
 }
+
 int main()
 {
 	int x[3] = { 1,2,3 };
