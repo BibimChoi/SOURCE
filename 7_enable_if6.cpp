d17c1997a29c914b943a158dc8b9297e5250115a
ecourse <smkang@codenuri.co.kr>
2021-03-23 14:31:46 +0900

    Add files via upload

--- 7_enable_if6.cpp
+++ 7_enable_if6.cpp
@@ -2,15 +2,20 @@
 #include <iostream>
 #include <type_traits>
 
-class Point
+template<typename T> class Point
 {
-	int x;
-	int y;
+	T x;
+	T y;
 public:
-	Point(int a, int b) : x(a), y(b) {}
+	Point(T a, T b) : x(a), y(b) {}
 };
 
 int main()
 {
-	Point p1(2.1, 3.4);
+	Point<double> p1(2.1, 3.4);
+	Point<int> p2(1, 2);
+
+	Point<double> p3 = p1; // ok
+	Point<double> p4 = p2; // 되는게 좋을까요 ? 안되는게 좋을까요 ?
+
 }
