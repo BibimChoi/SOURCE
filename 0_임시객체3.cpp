bb65e4270e4c1bf1f6c83c9f6f791bab843c90b3
ecourse <smkang@codenuri.co.kr>
2021-03-24 08:26:35 +0900

    Add files via upload

---
+++ 0_임시객체3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+class Point
+{
+public:
+	int x, y;
+
+	Point(int a, int b) { std::cout << "Point()" << std::endl; }
+	~Point() { std::cout << "~Point()" << std::endl; }
+};
+
+// 임시객체와 함수 인자
+//void foo(Point& pt) {} : 임시객체를 인자로 받을수 없다.!! 
+void foo(const Point& pt) {} // 임시객체를 받을수 있다.
+
+int main()
+{
+//	Point pt(1, 2);
+//	foo(pt);
+
+	// 임시객체를 사용한 인자 전달.
+	foo(Point( 1, 2 ));
+	// sort(v, std::less{}, // 이 표현이 결국 임시객체로 less를 전달.
+
+
+
+	std::cout << "-----" << std::endl;
+}
+
+
+
+
+
+
