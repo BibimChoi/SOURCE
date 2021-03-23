a133af5b0f8a4523b0fbbdd80cf8f72a278edbd9
ecourse <smkang@codenuri.co.kr>
2021-03-23 10:46:54 +0900

    Add files via upload

--- 4_type_traits7.cpp
+++ 4_type_traits7.cpp
@@ -2,21 +2,39 @@
 #include <iostream>
 #include <type_traits>
 
+
+
 template<typename T> void printv(const T& v)
 {
-	if ( std::is_pointer_v<T> )
-		std::cout << v << " : " << *v << std::endl;
+	// if : 컴파일시 false 가 되어도
+	//		"템플릿 ==> C++함수" 가 생성된다.(인스턴스화)
+	/*
+	if ( std::is_pointer_v<T> ) // if ( false )
+		std::cout << v << " : " << *v << std::endl; // 이 부분도 C++ 함수로 생성
+	else
+		std::cout << v << std::endl;
+	*/
+
+	// if constexpr : 조건이 false 일때는 해당 문장을 인스턴스화 하지 말라는 의미!
+	// C++17
+	if constexpr (std::is_pointer_v<T>)
+		std::cout << v << " : " << *v << std::endl; 
 	else
 		std::cout << v << std::endl;
 }
+// *v 가 에러가 나오는 현상에 대한 해결책.
+// 1. integral_constant - C++11
+// 2. enable_if         - C++11
+// 3. if constexpr      - C++17
+// 4. concept           - C++20
+
+
+
 
 int main()
 {
 	int n = 10;
-	double d = 3.4;
 
-	printv(n);
-	printv(d);
-	printv(&n);
+	printv(n); // 이 경우에 . error
 }
 
