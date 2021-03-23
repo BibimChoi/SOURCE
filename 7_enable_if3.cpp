40b406416c7a27bbce4a166c0fc1b22dee4f290a
ecourse <smkang@codenuri.co.kr>
2021-03-23 13:44:16 +0900

    Add files via upload

--- 7_enable_if3.cpp
+++ 7_enable_if3.cpp
@@ -1,8 +1,13 @@
 ﻿#include <iostream>
 #include <type_traits>
 
+// static_assert                        : 조건을 만족하지 않으면 무조건 에러
+// enable_if 를 사용한 반환 타입 만들기 : 조건을 만족하지 않으면 "함수생성안함"
+//								동일 이름의 다른 함수가 있다면 사용가능.
+
 template<typename T>
-typename std::enable_if< std::is_integral_v<T>, T>::type 
+//typename std::enable_if< std::is_integral_v<T>, T>::type 
+std::enable_if_t< std::is_integral_v<T>, T>
 gcd(T a, T b)
 {
 	//static_assert(std::is_integral_v<T>, "error, T is no integer");
