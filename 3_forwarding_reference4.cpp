f0d8cacfc6bbf5f69524392e3f5fe6c6ece8a597
ecourse <smkang@codenuri.co.kr>
2021-03-24 16:17:12 +0900

    Add files via upload

--- 3_forwarding_reference4.cpp
+++ 3_forwarding_reference4.cpp
@@ -10,10 +10,23 @@ public:
 	Data(Data&& d) { LOG_FUNCTION_NAME(); }
 	Data& operator=(Data&&) { LOG_FUNCTION_NAME(); return *this; }
 };
+
+template<typename T> T&& xmove(T& obj)
+{
+	return static_cast<T&&>(obj);
+}
+
+Data foo()
+{
+	Data d;
+	return d;
+}
+
 int main()
 {
 	Data d1;
 		
-	Data d2 = std::move(d1);
+	Data d2 = xmove(d1);
+	Data d2 = xmove(foo());
 	
 }
