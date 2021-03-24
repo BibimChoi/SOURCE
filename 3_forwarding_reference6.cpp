c3fda91c0cc657b7029a6d71fdcc0bfd3380207d
ecourse <smkang@codenuri.co.kr>
2021-03-24 16:49:11 +0900

    Add files via upload

--- 3_forwarding_reference6.cpp
+++ 3_forwarding_reference6.cpp
@@ -34,19 +34,34 @@ public:
 			printf("%p 버퍼의 %d번째를 %d 로 채우기\n", buff, idx, b);
 			return *this;
 		}
+		
+		operator bool()
+		{
+			std::cout << "buff 의 idx 를 조사해서 결과 반환" << std::endl;
+
+			return true;
+		}
 	};	   
 
 	BitProxy operator[](int idx) { return BitProxy(buff, idx); }
 };
 int main()
 {
-	Vector<int>  v1(10, 3);
-	Vector<bool> v2(10, false);
+//	Vector<int>  v1(10, 3);
+//	Vector<bool> v2(10, false);
+
+	std::vector<bool> v2(10, false);
 
 	v2[0] = false;
-//	bool b = v2[0];
+	bool b = v2[0];
+
 //	auto& r1 = v1[0]; // ok.    v1[0] 은 임시객체 아님.
 //	auto& r2 = v2[0]; // error. v2[0] 은 임시객체 반환.
+	auto&& r2 = v2[0]; // ok.. forwarding reference
+
+	auto a = v2[0];
+	std::cout << typeid(a).name() << std::endl;
+	
 }
 
 
