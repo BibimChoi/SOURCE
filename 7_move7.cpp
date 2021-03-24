b55eda591840874cf1cee49a33e3e7df523e3b5f
ecourse <smkang@codenuri.co.kr>
2021-03-24 13:02:15 +0900

    Add files via upload

---
+++ 7_move7.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+#include <string>
+
+class Object
+{
+public:
+	std::string name;
+
+	Object(std::string name) : name(name) {}
+};
+int main()
+{
+	Object o1("hello");
+	Object o2 = o1;
+//	Object o2 = std::move(o1);
+
+	std::cout << o1.name << std::endl;
+}
