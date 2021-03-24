18708248e75562d936ad112f32323a932ec8c631
ecourse <smkang@codenuri.co.kr>
2021-03-25 08:50:57 +0900

    Add files via upload

--- 0_unevaluated6.cpp
+++ 0_unevaluated6.cpp
@@ -8,9 +8,23 @@ public:
 	void resize() {}
 };
 // "Member Detect" 라는 C++ IDioms 입니다.
+// C++20 부터는 "Concept"을 사용하면 아주 간단해집니다.
+/*
+template<typename T> 
+concept has_resize = requires(T a)
+{
+	{ a.resize() };
+};
+int main()
+{
+	// concept이름<타입> 의 최종 결과는 bool 입니다.
+	bool b = has_resize<Test>;
+}
+*/
+
 
 using YES = int[2];
-using NO = int[1];
+using NO  = int[1];
 
 template<typename T> YES&  check(decltype(std::declval<T>().resize())* p);
 template<typename T> NO&   check(...);
