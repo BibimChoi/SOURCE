29f2799e0eb5d872e2c613a407ff089e154f52a0
ecourse <smkang@codenuri.co.kr>
2021-03-24 11:21:37 +0900

    Add files via upload

--- 7_move4.cpp
+++ 7_move4.cpp
@@ -20,7 +20,6 @@ public:
 		name = new char[strlen(c.name) + 1];
 		strcpy(name, c.name);
 	}
-
 	// 임시객체를 위한 복사 생성자를 
 	// 별도로 제공하자.
 	// "move 생성자라고 합니다."
@@ -29,9 +28,7 @@ public:
 		c.name = nullptr; // 원본 객체의 자원포인터 reset
 						// 이 코드가 되려면 임시객체도
 						// 상태를 변경할수 있어야합니다.
-						
 	}
-
 };
 
 Cat foo()
@@ -43,9 +40,17 @@ Cat foo()
 int main()
 {
 	Cat c1("nabi", 2);
-	Cat c2 = c1;
 
-	Cat c3 = foo(); // 이순간을 생각해 봅시다.
+	Cat c2 = c1;	// 복사 생성자 호출
+	Cat c3 = foo(); // move 생성자 호출
+
+	Cat c4 = static_cast<Cat&&>(c1); //  move 생성자를 호출하기 위해 
+									// lvalue 를 rvalue로 캐스팅 한다.
+
+	Cat c5 = std::move(c3); // 이  표준 함수가 위처럼 캐스팅하고 있습니다.
+	// 결국 : std::move()는 캐스팅에 불과 하고 
+	//        캐스팅 결과로 "move 생성자"가 호출되는데
+	//		  move 생성자 안에서 자원을 이동하는 코드를 만들어야 합니다.
 }
 
 
