1dcebc94abbc503da0b67ded6ae9d003dd569249
ecourse <smkang@codenuri.co.kr>
2021-03-24 11:15:38 +0900

    Add files via upload

--- 7_move4.cpp
+++ 7_move4.cpp
@@ -14,12 +14,24 @@ public:
 	}
 	~Cat() { delete[] name; }
 
-	// 깊은 복사로 구현한 복사 생성자
+	// 깊은 복사로 구현한 "복사 생성자"
 	Cat(const Cat& c) : age(c.age)
 	{
 		name = new char[strlen(c.name) + 1];
 		strcpy(name, c.name);
 	}
+
+	// 임시객체를 위한 복사 생성자를 
+	// 별도로 제공하자.
+	// "move 생성자라고 합니다."
+	Cat(Cat&& c) : age(c.age), name(c.name)
+	{
+		c.name = nullptr; // 원본 객체의 자원포인터 reset
+						// 이 코드가 되려면 임시객체도
+						// 상태를 변경할수 있어야합니다.
+						
+	}
+
 };
 
 Cat foo()
