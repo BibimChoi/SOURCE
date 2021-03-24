0247901778b2d4efdba04b70b74b7095b14fd745
ecourse <smkang@codenuri.co.kr>
2021-03-24 14:26:53 +0900

    Add files via upload

---
+++ 7_move12.cpp
@@ -0,0 +1,22 @@
+﻿#include "cppmaster.h"
+#include <vector>
+using namespace std;
+
+class Data
+{
+public:
+	Data() = default;
+	Data(const Data& d) { LOG_FUNCTION_NAME(); }
+	Data& operator=(const Data&) { LOG_FUNCTION_NAME(); return *this; }
+	Data(Data&& d) { LOG_FUNCTION_NAME(); }
+	Data& operator=(Data&&) { LOG_FUNCTION_NAME(); return *this; }
+};
+int main()
+{
+	Data d1;
+
+	std::vector<Data> v;
+	v.push_back(d1);
+	v.push_back(std::move(d1));
+
+}
