bbc146fde4e9026d31fbd435a71142b2119d4e6a
ecourse <smkang@codenuri.co.kr>
2021-03-24 15:40:37 +0900

    Add files via upload

---
+++ 3_forwarding_reference3.cpp
@@ -0,0 +1,21 @@
+﻿#include "cppmaster.h"
+
+template<typename T> void foo(T&& arg)
+{
+	// __FUNCTION__ : 함수의 이름을 담고 있는 매크로, C++ 표준
+	std::cout << __FUNCTION__ << std::endl;
+}
+int main()
+{
+	int n = 10;
+
+	foo(n);  // T=int&    T&&=int& && => int& 
+	foo(10); // T=int     T&&=int&&
+
+	const int c = 10;
+	foo(c); 
+}
+
+
+
+
