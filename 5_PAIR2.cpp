2ae6f32df8a88b411d9e5e8700f0e388d881cbf2
ecourse <smkang@codenuri.co.kr>
2021-03-25 16:24:58 +0900

    Add files via upload

--- 5_PAIR2.cpp
+++ 5_PAIR2.cpp
@@ -9,7 +9,20 @@ template<typename T, typename U> struct PAIR
 	U second;
 
 	// C++98 에서 사용하던 스타일
-	PAIR(const T& a, const U& b) : first(a), second(b) {}
+	//PAIR(const T& a, const U& b) : first(a), second(b) {}
+	//PAIR(T&& a, U&& b)       : first(std::move(a)), second(std::move(b)) {}
+	//PAIR(const T& a, U&& b)  : first(a),            second(std::move(b)) {}
+	// PAIR(T&& a, Uconst U& b) : first(std::move(a)), second(b) {}
+
+	// C++11 부터 사용되는 전형적인 모양의 생성자
+	template<typename A, typename B> 
+	PAIR(A&& a, B&& b)
+	{
+		// 다음중 맞는 것은 ?
+		first = a;
+		first = std::move(a);
+		first = std::forward(a);
+	}
 };
 
 int main()
