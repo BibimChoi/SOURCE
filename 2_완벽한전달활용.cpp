4db38b61ad35ae738bc955f562755db9a181115b
ecourse <smkang@codenuri.co.kr>
2021-03-25 11:15:33 +0900

    Add files via upload

--- 2_완벽한전달활용.cpp
+++ 2_완벽한전달활용.cpp
@@ -5,7 +5,7 @@ class Point
 {
 	int x, y;
 public:
-	Point(int a, int b) { LOG_FUNCTION_NAME(); }
+	Point(int* a, int& b) { LOG_FUNCTION_NAME(); }
 	~Point()            { LOG_FUNCTION_NAME(); }
 	Point(const Point&) { LOG_FUNCTION_NAME(); }
 };
@@ -23,9 +23,11 @@ int main()
 
 	// 3. 객체를 전달하지 말고..
 	//    객체를 만들기 위한 생성자 인자를 전달한다.
-	v.emplace_back(1, 2); 
+	int n = 0;
+	//v.emplace_back(0, n); 
+	v.emplace_back(nullptr, n);
 				// emplace_back() 함수가 내부적으로
-				// new Point(1,2) 를 수행..
+				// new Point(0, n) 를 수행..
 
 	// STL 컨테이너에
 	// 사용자 정의 타입을 값으로 보관하려면
