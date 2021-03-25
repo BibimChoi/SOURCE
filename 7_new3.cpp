269e0afd5f415399d88d8a0f5a611952b49424ce
ecourse <smkang@codenuri.co.kr>
2021-03-25 14:22:42 +0900

    Add files via upload

---
+++ 7_new3.cpp
@@ -0,0 +1,24 @@
+#include "cppmaster.h"
+#include <vector>
+
+/*
+class DBConnect {};
+
+int main()
+{
+	std::vector<int> v(10);
+
+	v.resize(7);
+	v.resize(8);
+}
+*/
+int main()
+{
+	std::vector<int> v(10, 3);
+
+	v.resize(7);  // 이 순간의 원리를 생각해 봅시다.
+}
+
+
+
+
