5a32957e25d764c85a305addc36995565350881f
ecourse <smkang@codenuri.co.kr>
2021-03-26 08:59:30 +0900

    Add files via upload

---
+++ 2_람다표현식1.cpp
@@ -0,0 +1,21 @@
+﻿#include <algorithm>
+#include <functional>  
+#include <vector>
+using namespace std;
+
+// 람다 표현식의 원리
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10 };
+
+//	std::sort(v.begin(), v.end(), 비교함수);
+
+	std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; } );
+}
+
+
+
+
+
+
