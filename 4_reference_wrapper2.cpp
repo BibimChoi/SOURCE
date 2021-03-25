fbf990c682d8556a3b9a6498f2708a8e2eb9b220
ecourse <smkang@codenuri.co.kr>
2021-03-25 13:25:35 +0900

    Add files via upload

---
+++ 4_reference_wrapper2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <functional>
+
+template<typename T> 
+struct reference_wrapper
+{
+	T* obj;
+public:
+	reference_wrapper(T& p) : obj(&p) {}
+
+	// 진짜 참조로 변환될수 있는 변환 연산자
+	operator T&() { return *obj; }
+};
+int main()
+{
+	int n1 = 10;
+	int n2 = 20;
+
+	reference_wrapper<int> r1(n1);
+	reference_wrapper<int> r2(n2);
+
+	r1 = r2;
+
+	// 진짜 참조 타입으로 암시적 변환되게 하자.!!
+	int& r = r1; // r1.operator int&() 
+
+
+	std::cout << n1 << std::endl;	
+	std::cout << n2 << std::endl;	
+	std::cout << r1 << std::endl;	
+	std::cout << r2 << std::endl;	
+}
