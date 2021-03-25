8a594b7e66783e6c5e4443a7aba30914d9ef85a9
ecourse <smkang@codenuri.co.kr>
2021-03-25 10:48:35 +0900

    Add files via upload

--- 3_완벽한전달_주의사항.cpp
+++ 3_완벽한전달_주의사항.cpp
@@ -20,7 +20,10 @@ int main()
 
 	// C++11 부터는 포인터 의미로 0을 사용하지 말고 nullptr을 사용하자.
 	foo(nullptr); // ok
-	chronometry(foo, nullptr); // ok
+	chronometry(foo, nullptr); // ok,   T=nullptr_t 로 결정된다.
+								// nullptr_t arg = nullptr;
+								// foo(arg);  가 되는데.. nullptr_t 타입의 모든 변수는
+								//						포인터로 암시적 변환 될수 있다.
 
 
 }
