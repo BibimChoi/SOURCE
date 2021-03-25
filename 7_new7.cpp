5b357741986e206b0b696972960a0f4e654758d4
ecourse <smkang@codenuri.co.kr>
2021-03-25 15:19:56 +0900

    Add files via upload

---
+++ 7_new7.cpp
@@ -0,0 +1,13 @@
+﻿#include <vector>
+#include <iostream>
+
+int main()
+{
+	std::vector<int> v;
+
+	std::cout << "---------------" << std::endl;
+	v.resize(10);
+	std::cout << "---------------" << std::endl;
+	v.resize(5);
+	std::cout << "---------------" << std::endl;
+}
