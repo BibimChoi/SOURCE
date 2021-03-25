fe0bb0f6b4a9e84df1546fc510d2c15734bfa2b7
ecourse <smkang@codenuri.co.kr>
2021-03-26 08:24:38 +0900

    Add files via upload

---
+++ 1_함수객체3.cpp
@@ -0,0 +1,44 @@
+﻿// 1. 일반 함수는 자신만의 타입이 없습니다.
+//    signature(리턴타입과 파라미터) 가 
+//    동일한 모든 함수는 같은 타입입니다.
+
+// 2. 함수객는 자신만의 타입이 있습니다.
+//    signature(리턴타입과 파라미터) 가 
+//    동일해도 모든 함수객체는 다른 타입 이다.
+
+
+
+struct Less
+{
+	inline bool operator()(int a, int b) { return a < b; }
+};
+struct Greater
+{
+	inline bool operator()(int a, int b) { return a > b; }
+};
+//--------------------------------------
+// 비교정책을 변경할수 있는데, 비교 정책이 인라인 치환이 되는 sort 만들기
+// "템플릿 과 함수 객체" 를 사용한 기술
+// STL 의 중요한 설계 원칙 중 하나
+template<typename T>
+void Sort(int* x, int sz, T cmp)
+{
+	for (int i = 0; i < sz - 1; i++)
+	{
+		for (int j = i + 1; j < sz; j++)
+		{
+			if (cmp(x[i], x[j]))    
+				std::swap(x[i], x[j]);
+		}
+	}
+}
+int main()
+{
+	int x[10] = { 1,3,5,7,9,2,4,6,8,10 };
+
+	Less    f1; f1(1, 2); Sort(x, 10, f1); // ok
+	Greater f2; f2(1, 2); Sort(x, 10, f2); // ?
+}
+
+
+
