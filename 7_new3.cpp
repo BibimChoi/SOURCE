a23fbf2430601f4ebcca30ed16327b9d2ac00c7f
ecourse <smkang@codenuri.co.kr>
2021-03-25 14:28:05 +0900

    Add files via upload

--- 7_new3.cpp
+++ 7_new3.cpp
@@ -18,9 +18,27 @@ int main()
 
 	v.resize(7);  // 이 순간의 원리를 생각해 봅시다.
 
-	std::cout << v.size() << std::endl;    // 7
-	std::cout << v.capacity() << std::endl;// 10
+	std::cout << v.size()     << std::endl; // 7
+	std::cout << v.capacity() << std::endl; // 10
+
+	v.push_back(0); // 끝에 한개 추가
+
+	std::cout << v.size()     << std::endl; // 8
+	std::cout << v.capacity() << std::endl; // 10
+
+	v.shrink_to_fit(); // 여분의 메모리 제거
+
+	std::cout << v.size() << std::endl; // 8
+	std::cout << v.capacity() << std::endl; // 8
+
+
+	v.push_back(0); // 끝에 한개 추가
+
+	std::cout << v.size() << std::endl; // ?
+	std::cout << v.capacity() << std::endl; // ?
+
 }
+
 
 
 
