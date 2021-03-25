b08705bfa59a8ab30c88939f4ec399a1bd11f5f3
ecourse <smkang@codenuri.co.kr>
2021-03-25 14:37:55 +0900

    Add files via upload

--- 7_new3.cpp
+++ 7_new3.cpp
@@ -1,17 +1,29 @@
 #include "cppmaster.h"
 #include <vector>
 
-/*
+// DBConnect 는 생성자에서 DB에 접속한다고 가정해 보세요
 class DBConnect {};
 
 int main()
 {
-	std::vector<int> v(10);
+	std::vector<DBConnect> v(5);
 
-	v.resize(7);
-	v.resize(8);
+	v.resize(3); // 이순간 줄어든 2개를 위한 메모리는 계속 존재하지만
+				 // 2개의 객체의 소멸자는 호출되어야 합니다.
+				 // 소멸자의 명시적 호출이 필요 합니다.
+
+	v.resize(4); // 늘어난 추가의 메모리는 이미 존재 합니다.
+				 // 하지만, 생성자를 명시적으로 호출에서
+				 // DB에 접속해야 합니다
+				 // 생성자의 명시적 호출이 필요 합니다.
 }
-*/
+
+
+
+
+
+
+/*
 int main()
 {
 	std::vector<int> v(10, 3);
@@ -32,13 +44,15 @@ int main()
 	std::cout << v.capacity() << std::endl; // 8
 
 
-	v.push_back(0); // 끝에 한개 추가
+	v.push_back(0); // 끝에 한개 추가 - VC : 12
 
-	std::cout << v.size() << std::endl; // ?
-	std::cout << v.capacity() << std::endl; // ?
+	std::cout << v.size() << std::endl; // 9
+	std::cout << v.capacity() << std::endl; // 12
 
+//	st = [1, 2, 3, 4];
+//	st.append(10) // 내부적으로 얼마나 늘리까요 ? 8
 }
-
+*/
 
 
 
