2a2432f07462b4528873a74b1f64cc31138be01d
ecourse <smkang@codenuri.co.kr>
2021-03-25 16:40:40 +0900

    Add files via upload

--- 5_PAIR3.cpp
+++ 5_PAIR3.cpp
@@ -8,9 +8,11 @@ template<typename T, typename U> struct PAIR
 	T first;
 	U second;
 
+	// std::decay_t<A> : A 에서, const, volatile, reference를 제거한 타입 꺼내기
 	template<typename A, 
 		     typename B,
-			 typename = std::enable_if_t< std::is_same_v<A, T> && std::is_same_v<B, U> >>
+			 typename = std::enable_if_t< std::is_same_v< std::decay_t<A>, T> && 
+										  std::is_same_v< std::decay_t<B>, U> >>
 	PAIR(A&& a, B&& b) : first(std::forward<A>(a)), second(std::forward<B>(b))
 	{
 	}
@@ -21,9 +23,10 @@ int main()
 	std::string s1 = "AA";
 	std::string s2 = "BBBB";
 
-	PAIR<std::string, std::string> p1(s1, s2);
+	PAIR<std::string, std::string> p1(s1, s2);	// 이 순간 T=string, A=string&
+
 	PAIR<std::string, std::string> p2(s1, std::move(s2));
 
-	PAIR<int*, int*> p3(1, 2);
+	//PAIR<int*, int*> p3(1, 2);
 }
 
