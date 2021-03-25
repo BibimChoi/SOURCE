aaa127f028a6b9c09baaa3b2b47904e349ee8ea5
ecourse <smkang@codenuri.co.kr>
2021-03-25 10:35:33 +0900

    Add files via upload

---
+++ 3_완벽한전달_주의사항2.cpp
@@ -0,0 +1,30 @@
+﻿// 9_완벽한전달_주의사항
+#include <iostream>
+
+template<typename F, typename T>
+void chronometry(F f, T&& arg)
+{
+	f(std::forward<T>(arg));
+}
+void foo() {}
+void foo(int a) {}
+void foo(int a, int b) {}
+
+int main()
+{
+	// 함수를 직접 호출하면 ok
+	foo();
+	foo(10);
+
+	// 그런데...
+	chronometry(foo, 1, 2); // ?
+}
+
+
+
+
+
+
+
+
+
