a52485f968c451d068427db56a9e46769ecc3963
ecourse <smkang@codenuri.co.kr>
2021-03-25 16:35:25 +0900

    Add files via upload

---
+++ 5_PAIR3.cpp
@@ -0,0 +1,29 @@
+// 5_PAIR2
+#include <iostream>
+#include <type_traits>
+#include <string>
+
+template<typename T, typename U> struct PAIR
+{
+	T first;
+	U second;
+
+	template<typename A, 
+		     typename B,
+			 typename = std::enable_if_t< std::is_convertible_v<A, T> && std::is_convertible_v<B, U> >>
+	PAIR(A&& a, B&& b) : first(std::forward<A>(a)), second(std::forward<B>(b))
+	{
+	}
+};
+
+int main()
+{
+	std::string s1 = "AA";
+	std::string s2 = "BBBB";
+
+	PAIR<std::string, std::string> p1(s1, s2);
+	PAIR<std::string, std::string> p2(s1, std::move(s2));
+
+	PAIR<int*, int*> p3(1, 2);
+}
+
