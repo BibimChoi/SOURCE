3e7b7015bb860c5da0fba5fa495f2781678e23f9
ecourse <smkang@codenuri.co.kr>
2021-03-25 11:25:49 +0900

    Add files via upload

--- 2_완벽한전달활용2.cpp
+++ 2_완벽한전달활용2.cpp
@@ -15,10 +15,22 @@ public:
 	Point(int a, int b) {  }
 };
 
+// std::make_shared 의 원리
+template<typename T> 
+std::shared_ptr<T> make_shared()
+{
+	T* p = new T();
+}
+
+
 int main()
 {
 	// 아래 코드는 동적 메모리 할당이 몇번 발생할까요 ?
-	std::shared_ptr<Point> sp(new Point(1, 2));
+	//std::shared_ptr<Point> sp(new Point(1, 2));
+
+	// 아래 코드는 
+	// sizeof(Point) + sizeof(관리객체) 를 한번에 메모리 할당하게 됩니다
+	std::shared_ptr<Point> sp = std::make_shared<Point>(1, 2);
 }
 
 
