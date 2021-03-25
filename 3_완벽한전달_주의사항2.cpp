0beafb569b558d70e8f47d8a3da927cbc3bc5c99
ecourse <smkang@codenuri.co.kr>
2021-03-25 10:37:28 +0900

    Add files via upload

--- 3_완벽한전달_주의사항2.cpp
+++ 3_완벽한전달_주의사항2.cpp
@@ -17,7 +17,11 @@ int main()
 	foo(10);
 
 	// 그런데...
-	chronometry(foo, 1, 2); // ?
+	//chronometry(foo, 1); // error. 어떤 foo인지 알수 없다.
+
+	chronometry(static_cast<void(*)(int)>(foo), 1); // ok.. 컴파일러가 함수를 찾을수 있도록
+													// 함수 모양의 정보를 알려준다.
+												// 일반적으로 캐스팅 모양 사용
 }
 
 
