9d5cc982d1ee4aeadfbfbfc96ed4149a1ff78dde
ecourse <smkang@codenuri.co.kr>
2021-03-25 11:11:17 +0900

    Add files via upload

--- 2_완벽한전달활용.cpp
+++ 2_완벽한전달활용.cpp
@@ -27,6 +27,12 @@ int main()
 				// emplace_back() 함수가 내부적으로
 				// new Point(1,2) 를 수행..
 
+	// STL 컨테이너에
+	// 사용자 정의 타입을 값으로 보관하려면
+	// push_xxx 대신 emplace_xxx 를 사용해라
+	// push_front => emplace_front
+	// push_back  => emplace_back
+	// insert     => emplace
 
 
 	std::cout << "-----" << std::endl;
