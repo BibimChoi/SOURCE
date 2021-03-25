be886b8940a9676282a433778ebe0bfc690dc28b
ecourse <smkang@codenuri.co.kr>
2021-03-25 10:15:06 +0900

    Add files via upload

--- 1_완벽한전달9.cpp
+++ 1_완벽한전달9.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <functional> // std::invoke()
+#include <functional> 
 
 template<typename F, typename ... Ts>
 decltype(auto) chronometry(F f, Ts&& ... args)
@@ -7,9 +7,18 @@ decltype(auto) chronometry(F f, Ts&& ... args)
 	return std::invoke(f, std::forward<Ts>(args)...);
 }
 
+struct Add
+{
+	int operator()(int a, int b) const
+	{
+		return a + b;
+	}
+};
+
 int main()
 {
-	int n = add(1, 2);
+	Add add;
+	int n = add(1, 2); // add.operator()(1, 2)ÀÇ ¿ø¸®
 }
 
 
