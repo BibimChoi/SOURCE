cfa28277befb4ac2db767d4a8c891d13ac0f2517
ecourse <smkang@codenuri.co.kr>
2021-03-25 14:45:45 +0900

    Add files via upload

--- 7_new4.cpp
+++ 7_new4.cpp
@@ -15,7 +15,7 @@ template<typename T> class vector
 	int size;
 	int capacity;
 public:
-	vector(int sz) : size(sz), capacity(sz)
+	vector(int sz, const T& value = T()) : size(sz), capacity(sz)
 	{
 		// 아래처럼 만들면 디폴트 생성자가 있는 타입만 저장할수 있습니다.
 		// buff = new T[sz];
@@ -23,12 +23,20 @@ public:
 		// 1. 메모리만 할당
 		buff = static_cast<T*>(operator new(sizeof(T)*sz));
 
+		// 2. 생성자 호출
+		for (int i = 0; i < sz; i++)
+		{
+			// new(&buff[i]) T; // 디폴트 생성자 호출
+			new(&buff[i]) T(value); // ok.. 복사 생성자 사용
+		}
 
 	}
 	~vector() {}
 };
 int main()
 {
-	vector<Point> v(10);
+//	Point pt(1, 2);
+//	vector<Point> v(10, pt);
+	vector<Point> v(10, Point(1,2) );
 
 }
