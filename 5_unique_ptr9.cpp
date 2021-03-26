94af30eb05e7331e6e9e2f54656aeb7fa24a4cc4
ecourse <smkang@codenuri.co.kr>
2021-03-26 14:43:40 +0900

    Add files via upload

--- 5_unique_ptr9.cpp
+++ 5_unique_ptr9.cpp
@@ -20,11 +20,17 @@ int main()
 
 // 아래 코드에서 반환 타입을 스마트 포인터로 변경하려고 합니다.
 // shared vs unique 중 선택해 보세요
-int* Alloc(int sz)
+// shared_ptr 로 반환하면 : shared_ptr 로만 받을수 있습니다.
+// unique_ptr 로 반환하면 : 받는 쪽에서 shared_ptr, unique_ptr을 선택할수 있습니다.
+//						    반환 객체가 "unique_ptr"의 임시객체라는 것이 핵심.
+//std::shared_ptr<int> Alloc(int sz)
+std::unique_ptr<int> Alloc(int sz)
 {
 	return static_cast<int*>(malloc(sz));
 }
 
+std::shared_ptr<int> ret1 = Alloc(100);
+std::unique_ptr<int> ret2 = Alloc(100); // shared 리턴 하면 에러
 
 
 
