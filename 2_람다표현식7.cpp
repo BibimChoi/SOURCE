e8f39f2d40c737e72b98f652f05e6a8e719bb454
ecourse <smkang@codenuri.co.kr>
2021-03-26 09:45:13 +0900

    Add files via upload

--- 2_람다표현식7.cpp
+++ 2_람다표현식7.cpp
@@ -14,6 +14,11 @@ struct Freer
 };
 int main()
 {
-	unique_ptr<int, Freer> up;
+//	unique_ptr<int, Freer> up;
+//	unique_ptr<int, [](void* p) {free(p); }  > up; // 람다표현식의 결과 => 객체
+
+	// "람다표현식은 평가되지 않은 표현식안에는 넣을수 없다."
+	// 단, C++20 부터는 넣을수 있다.
+	unique_ptr<int, decltype([](void* p) {free(p); }) > up;
 
 }
