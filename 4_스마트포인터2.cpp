54e14b6b8bc39fb6ba935e5f478d3fdc99f2ebb4
ecourse <smkang@codenuri.co.kr>
2021-03-26 10:31:48 +0900

    Add files via upload

---
+++ 4_스마트포인터2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// 1. 템플릿으로 만들어야 한다.
+
+template<typename T> class Ptr
+{
+	T* obj;
+public:
+	Ptr(T* p = 0) : obj(p) {}
+
+	~Ptr() { delete obj; }
+
+	T* operator->() { return obj; }
+	T& operator*() { return *obj; }
+};
+int main()
+{
+	Ptr<int> p1 = new int;
+	*p1 = 10;
+	std::cout << *p1 << std::endl;
+
+	Ptr<int> p2 = p1; // 런타입 에러.!!
+						// 얕은 복사!!
+}
+
+
