fc82546fd61389c3f9638ee6ed1fc419cc245a5c
ecourse <smkang@codenuri.co.kr>
2021-03-26 09:33:33 +0900

    Add files via upload

--- 2_람다표현식6.cpp
+++ 2_람다표현식6.cpp
@@ -1,7 +1,22 @@
+// generic lambda의 원리
+class ClosureType
+{
+public:
+	template<typename T, typename U>
+	inline auto operator()(T a, U b) const
+	{
+		return a + b;
+	}
+};
+
 int main()
 {
-	auto f = [](int a, int b) { return a + b; };
+//	auto f = [](int a, int b) { return a + b; };
+
+	// C++14에서 추가된 generic lambda
+	auto f = [](auto a, auto b) { return a + b; };
 
 	f(1, 2);
 	f(3.4, 2.1);
+	f(3.4, 2);
 }
