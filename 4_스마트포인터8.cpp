88b0bc5c5739b3d41b752222ca41057c1b638c4b
ecourse <smkang@codenuri.co.kr>
2021-03-26 11:23:48 +0900

    Add files via upload

--- 4_스마트포인터8.cpp
+++ 4_스마트포인터8.cpp
@@ -9,7 +9,7 @@ struct People
 	People(std::string n) : name(n) {}
 	~People() { std::cout << name << " 파괴" << std::endl; }
 
-	std::shared_ptr<People> bf; // best friend
+	std::shared_ptr<People> bf;
 };
 
 int main()
@@ -17,8 +17,6 @@ int main()
 	std::shared_ptr<People> sp1(new People("kim"));
 	std::shared_ptr<People> sp2(new People("lee"));
 
-	// 아래 코드 때문에 "상호 참조","순환 참조" 가 발생합니다.
-	// 자원이 삭제될수 없습니다. 자원 누수
 	sp1->bf = sp2;
 	sp2->bf = sp1;
 }
