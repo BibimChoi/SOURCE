984a9693b98ca00a16bb221c01cf13efef938d3c
ecourse <smkang@codenuri.co.kr>
2021-03-26 15:13:01 +0900

    Add files via upload

--- 6_checked_delete3.cpp
+++ 6_checked_delete3.cpp
@@ -11,16 +11,22 @@ struct default_delete
 		static_assert(sizeof(T) > 0, "Error, delete imcomplete type");
 		delete p;
 	}
-};
+	
+	// generic 복사생성자
+	template<typename U,
+		     typename = std::enable_if_t< std::is_constructible_v<U*, T*> >>
+	default_delete(const default_delete<U>&) {}
 
-void foo(default_delete<Dog> d)
+	default_delete() = default;
+};
+void foo(default_delete<Dog> d) // default_delete<Dog> d = default_delete<int>
 {
 	Dog* p = new Dog;
 	d(p);  // Dog를 지우는 삭제자
 }
-
 int main()
 {
-	default_delete<Dog> d;
+	//default_delete<Dog> d;
+	default_delete<Animal> d; // operator()(Animal* p) 의 모양임.
 	foo(d);
 }
