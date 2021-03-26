5c6f8cc4bfbfe8293d221de32cba667ba36f0576
ecourse <smkang@codenuri.co.kr>
2021-03-26 09:05:56 +0900

    Add files via upload

---
+++ 2_람다표현식2.cpp
@@ -0,0 +1,22 @@
+﻿#include <algorithm>
+#include <functional>  
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int v1 = 10, v2 = 20;
+
+	// 람다표현식은 auto 변수에 담아서 함수 처럼 사용해도 됩니다.
+	auto f = [](int a, int b) {return a + b; };
+
+
+
+}
+
+
+
+
+
+
