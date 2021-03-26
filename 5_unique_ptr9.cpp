0a4508ee3b0bde6adf62136bee91df1b7d72ace9
ecourse <smkang@codenuri.co.kr>
2021-03-26 14:39:15 +0900

    Add files via upload

--- 5_unique_ptr9.cpp
+++ 5_unique_ptr9.cpp
@@ -11,10 +11,22 @@ int main()
 	std::shared_ptr<int> sp(new int);
 	std::unique_ptr<int> up(new int);
 
-	std::shared_ptr<int> sp1 = up;				// 1
-	std::shared_ptr<int> sp2 = std::move(up);	// 2
+	std::shared_ptr<int> sp1 = up;				// 1. error
+	std::shared_ptr<int> sp2 = std::move(up);	// 2. ok
 
-	std::shared_ptr<int> up1 = sp;				// 3
-	std::shared_ptr<int> up2 = std::move(sp);	// 4
-
+	std::unique_ptr<int> up1 = sp;				// 3. error
+	std::unique_ptr<int> up2 = std::move(sp);	// 4. error
 }
+
+// 아래 코드에서 반환 타입을 스마트 포인터로 변경하려고 합니다.
+// shared vs unique 중 선택해 보세요
+int* Alloc(int sz)
+{
+	return static_cast<int*>(malloc(sz));
+}
+
+
+
+
+
+
