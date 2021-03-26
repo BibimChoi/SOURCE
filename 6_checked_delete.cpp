07e134bea1ce8152da6285db7eb248596bd8738a
ecourse <smkang@codenuri.co.kr>
2021-03-26 15:02:38 +0900

    Add files via upload

---
+++ 6_checked_delete.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+class Test; // 클래스 전방 전언
+			// 불완전타입(incomplete type)
+			// 불완전 타입은 delete 시 소멸자가 호출되지 않는다.
+
+class Test
+{
+public:
+	Test()  { std::cout << "Test()" << std::endl; }
+	~Test() { std::cout << "~Test()" << std::endl; }
+};
+
+void foo(Test* p)
+{
+	static_assert(sizeof(Test) > 0, "error, delete incomplete type");
+	delete p;
+}
+
+int main()
+{
+	Test* p = new Test;
+	foo(p);
+}
