c258e479b13e1c4cfee5fb0fc9088461a4ab0cdf
ecourse <smkang@codenuri.co.kr>
2021-06-01 15:48:40 +0900

    Update edit4.cpp

--- 오후수업/DAY2/edit4.cpp
+++ 오후수업/DAY2/edit4.cpp
@@ -59,6 +59,7 @@ public:
 	{
 		return s.size() < size && isdigit(c);
 	}
+
 	bool iscomplete(std::string s) override
 	{
 		return s.size() == size;
@@ -71,11 +72,29 @@ int main()
 
 	LimitDigitValidator v(5); // 값의 유효성을 확인하는 객체를 생성후
 	e.setValidator(&v);       // edit 와 연결
-	
+
+	LimitDigitValidator v2(15);
+	e.setValidator(&v2);       
+
 	while (1)
 	{
 		std::cout << e.getData() << std::endl;
 	}
 }
+// 핵심 정리 : 변하지 않은 흐름속에 변하는 것은 분리하세요
+//			   (공통성과 가변성을 분리)
 
+// 1. 변하는 것을 가상함수로 분리 => 실행시간 교체는 불가능하고, 정책의 재사용성도 힘듬..
+
+// 2. 변하는 것을 다른 클래스로 분리 => 실행시간 교체가능, 정책을 다른 클래스에서 사용가능
+
+// Edit3.cpp 에서
+//NumEdit e1;
+//AddressEdit e2; // e1이 정책을 변경한것이 아니라.. 다른 정책을 가진 다른 editbox
+				// 즉, 2개의 edit 박스
+				// 다른 정책을 가진 또다른 Edit를 "코딩시간"에 만들어서 사용하는 것
+// Edit4.cpp
+//Edit e;
+//e.setValidator(&v1);
+//e.setValidator(&v2); // 같은 editbox를 실행시간에 정책만 교체해서 사용
 
