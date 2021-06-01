5da1510b9ff3e6bd98630361fbac4c490a1e290c
ecourse <smkang@codenuri.co.kr>
2021-06-01 11:30:39 +0900

    Update policy4.cpp

--- DAY2/policy4.cpp
+++ DAY2/policy4.cpp
@@ -2,7 +2,18 @@
 // 방법 3. 변하는 정책 클래스를 인터페이스가 아닌 템플릿 인자로 변경할수 있게 한다.
 // => Policy Base Design(단위 전략) 이라는 기술
 
-template<typename T, typename ThreadModel > class List
+// 정책 클래스를 교체하는 기술
+
+//					인터페이스 기반 교체				템플릿 기반 교체
+// 성능				가상함수기반 - 느리다				인라인 함수 가능 - 빠르다
+// 실행시간 교체		실행시간 정책 교체 가능			실행시간 교체 안됨.
+// 약속된 함수이름	추상클래스 문법으로 코드로 표현	문서화 되어야 한다
+//													"모든 동기화 정책은 lock/unlock 필요"
+//													"named requierment 개념"
+
+
+
+template<typename T, typename ThreadModel = NoLock > class List
 {
 	ThreadModel tm;
 public:
@@ -30,7 +41,10 @@ public:
 };
 //----------------------------------------------
 // 이제 List 사용시 "템플릿 인자로 동기화정책"을 전달합니다
-List<int, NoLock> st; 
+//List<int, NoLock> st; 
+//List<int, MutexLock> st;
+
+List<int> st;
 
 int main()
 {
