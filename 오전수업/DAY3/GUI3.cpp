02604193bf35df1f11c732018bbd14c408f3d4f1
ecourse <smkang@codenuri.co.kr>
2021-06-02 10:49:46 +0900

    Create GUI3.cpp

---
+++ 오전수업/DAY3/GUI3.cpp
@@ -0,0 +1,78 @@
+#define USING_GUI
+#include "cppmaster_gui.h"
+#include <map>
+#include <vector>
+
+// MessageMap
+
+class CWnd;
+map<int, CWnd*> this_map;
+
+
+struct AFX_MSG
+{
+	int messageno;				// 메세지 번호
+	void (CWnd::*handler)();	// 메세지를 처리할 함수
+};
+
+
+
+class CWnd
+{
+	int mHandle;
+public:
+	void Create()
+	{
+		mHandle = ec_make_window(foo, "A");
+		this_map[mHandle] = this;
+	}
+
+	// 아래 가상 함수에서 메세지 처리 정보를 담은 배열을 반환해야합니다
+	virtual AFX_MSG* GetMessageMap()
+	{
+		return nullptr;
+	}
+
+	static int foo(int hwnd, int msg, int a, int b)
+	{
+		CWnd* const pThis = this_map[hwnd];
+
+		if (pThis == nullptr) return 0;
+
+		// 메세지 처리 정보를 담은 배열을 얻어 옵니다.
+		AFX_MSG* arr = pThis->GetMessageMap();
+
+		if (pThis == nullptr) return 0; // 처리할 메세지 없는 경우
+
+		while (arr->messageno)
+		{
+			if (arr->messageno == msg)
+			{
+				void(CWnd::*handler)() = arr->handler;
+				(pThis->*handler)(); // 멤버 함수 호출 - thiscall2 번 참고
+			}
+			++arr;
+		}
+		return 0;
+	}
+};
+
+
+
+
+class MyWindow : public CWnd
+{
+public:
+	void LButtonDown() { cout << "LBUTTON" << endl; }
+};
+
+int main()
+{
+	MyWindow w;
+	w.Create();
+
+	ec_process_message();
+}
+
+
+
