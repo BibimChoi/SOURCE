dfa18d42f019bf12de2df67b01729fbd32dffa93
ecourse <smkang@codenuri.co.kr>
2021-06-03 09:44:04 +0900

    Create menuevent2.cpp

---
+++ 오전수업/DAY4/menuevent2.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h> 
+#include <memory> // 스마트 포인터!
+
+class BaseMenu
+{
+	std::string title;
+public:
+	BaseMenu(std::string s) : title(s) {}
+
+	std::string getTitle() const { return title; }
+
+	virtual void command() = 0;
+};
+
+
+
+class PopupMenu : public BaseMenu
+{
+	std::vector< std::shared_ptr<BaseMenu> > v;
+public:
+	PopupMenu(std::string s) : BaseMenu(s) {}
+
+	void addMenu(std::shared_ptr<BaseMenu> p) { v.push_back(p); }
+
+	void command() override
+	{
+		while (1)
+		{
+			system("cls");
+			int sz = v.size();
+
+			for (int i = 0; i < sz; i++)
+			{
+				std::cout << i + 1 << ". " << v[i]->getTitle() << std::endl;
+			}
+			std::cout << sz + 1 << ". 상위 메뉴로 이동" << std::endl;
+
+			int cmd;
+			std::cout << "메뉴를 선택해 주세요 >> ";
+			std::cin >> cmd;
+
+			if (cmd == sz + 1)
+				break;
+
+			if (cmd < 1 || cmd > sz + 1)
+				continue;
+
+			v[cmd - 1]->command();
+
+		}
+	}
+};
+//-----------------------------------------------------
+// 메뉴 이벤트 처리 방법 1. 변하는 것을 가상함수로!
+class MenuItem : public BaseMenu
+{
+	int id;
+public:
+	MenuItem(std::string s, int n) : BaseMenu(s), id(n) {}
+
+	void command() override
+	{
+		OnCommand();
+	}
+	virtual void OnCommand() {}
+};
+// MenuItem 을 직접 사용하지말고 파생클래스를 만들라는 의미!!
+class RedMenu : public MenuItem
+{
+public:
+	void OnCommand() { std::cout << "색상을 Red 로 변경" << std::endl; }
+};
+class BlueMenu : public MenuItem
+{
+public:
+	void OnCommand() { std::cout << "색상을 Blue 로 변경" << std::endl; }
+};
+
+int main()
+{
+	std::shared_ptr<MenuItem> p1 = std::make_shared<RedMenu>("RED", 11);
+	std::shared_ptr<MenuItem> p2 = std::make_shared<BlueMenu>("BLUE", 12);
+
+	p1->command();
+	p2->command();
+}
+
+
+
+
+
+
+
