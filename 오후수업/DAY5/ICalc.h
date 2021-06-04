12668a407d0bc0c2efb42fcf694ceb86a65f3d7c
ecourse <smkang@codenuri.co.kr>
2021-06-04 16:34:05 +0900

    Update ICalc.h

--- 오후수업/DAY5/ICalc.h
+++ 오후수업/DAY5/ICalc.h
@@ -1,7 +1,19 @@
 // ICalc.h 
 #pragma once
 
-struct ICalc
+// 참조계수용 함수는 모든 종류의 Proxy 에서 사용될수 있다.
+// 기반 클래스(인터페이스)로 제공하자.
+struct IRef
+{
+	// Proxy 객체를 참조계수로 관리하기로 했다면
+	// 참조계수 관련 함수는 인터페이스에 있어야 합니다.
+	virtual void AddRef()  const = 0;
+	virtual void Release() const = 0;
+
+	virtual ~IRef() {}
+};
+// 이제, 다양한 종류의 Proxy 인터페이스 만들때 IRef에서 상속받으세요
+struct ICalc : public IRef
 {
 	virtual int Add(int a, int b) = 0;
 	virtual int Sub(int a, int b) = 0;
