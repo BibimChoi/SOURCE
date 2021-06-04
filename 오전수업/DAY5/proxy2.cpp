8412e29952237f7db8043cc23a59761f2554f517
ecourse <smkang@codenuri.co.kr>
2021-06-04 10:30:18 +0900

    Create proxy2.cpp

---
+++ 오전수업/DAY5/proxy2.cpp
@@ -0,0 +1,28 @@
+#define USING_GUI
+#include "cppmaster.h"
+
+// 서버를 대신하는 클래스를 만들어 봅시다.
+class Calc
+{
+	int server;
+public:
+	Calc() { server = ec_find_server("Calc"); }
+
+	int Add(int a, int b) { return ec_send_server(server, 1, a, b); }
+	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); }
+};
+
+int main()
+{
+	Calc* pCalc = new Calc;
+
+	std::cout << pCalc->Add(10, 20) << std::endl;
+	std::cout << pCalc->Sub(10, 20) << std::endl;
+}
+
+
+
+
+
+
+
