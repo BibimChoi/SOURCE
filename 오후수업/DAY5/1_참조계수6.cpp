ec47ba452a61530ef32330401ff383e063f9a334
ecourse <smkang@codenuri.co.kr>
2021-06-04 14:33:24 +0900

    Update 1_참조계수6.cpp

--- 오후수업/DAY5/1_참조계수6.cpp
+++ 오후수업/DAY5/1_참조계수6.cpp
@@ -28,19 +28,33 @@ public:
 	void Go() { std::cout << "Go" << std::endl; }
 };
 
+// AddRef/Release를 자동으로 호출하는 스마트 포인터를 만들어 봅시다.
+template<typename T> 
+class AutoPtr
+{
+	T* obj;
+public:
+	AutoPtr(T* p = nullptr) : obj(p) {}
+	AutoPtr(const AutoPtr<T>& ap) : obj(ap.obj) {}
+	~AutoPtr() { }
+};
 
 int main()
 {
-	Truck* p1 = new Truck;
-	p1->AddRef();
+//	Truck* p1 = new Truck;
+//	p1->AddRef();
 
-	Truck* p2 = p1;
-	p2->AddRef();
+	AutoPtr<Truck> p1 = new Truck; // AutoPtr<Truck> p1(new Truck);
+
+//	Truck* p2 = p1;
+//	p2->AddRef();
+	AutoPtr<Truck> p2 = p1;
+
 
 	p1->Go();
 
-	p2->Release();
-	p1->Release();
+//	p2->Release();
+//	p1->Release();
 }
 
 
