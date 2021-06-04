9e01f87803a33f718cd0ed8cd9b90b84c69a1000
ecourse <smkang@codenuri.co.kr>
2021-06-04 13:34:36 +0900

    Update 1_참조계수2.cpp

--- 오후수업/DAY5/1_참조계수2.cpp
+++ 오후수업/DAY5/1_참조계수2.cpp
@@ -11,6 +11,9 @@ public:
 	void AddRef()  { ++refCount; }
 	void Release() { if (--refCount == 0) delete this; }
 
+	// protected 소멸자는 외부에서 호출할수 없습니다.
+	// 따라서, 외부에서 delete 하는 코드가 있으면 에러 입니다.
+protected:
 	~Car() { cout << "Car 파괴" << endl; }
 };
 
@@ -20,7 +23,9 @@ int main()
 	p1->AddRef();		// 규칙 1. 객체를 만들면 참조 계수 증가
 
 	Car* p2 = p1; 
-	p2->AddRef();		// 규칙 2. 객체 주소를 복사하면 참조계수 감소
+	p2->AddRef();		// 규칙 2. 객체 주소를 복사하면 참조계수 증가
+
+//	delete p2;
 
 	// 규칙 3. 포인터 사용후에 참조계수 감소
 	p2->Release();
