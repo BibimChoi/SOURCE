9192a83b537cde7105535b87332c4e127acd910f
ecourse <smkang@codenuri.co.kr>
2021-06-07 09:21:53 +0900

    Update Decorator7.cpp

--- 오전수업/DAY6/Decorator7.cpp
+++ 오전수업/DAY6/Decorator7.cpp
@@ -61,14 +61,26 @@ int main()
 	FileStream fs("a.txt");
 	fs.Write("Hello");   
 
+//	XmlStream xs(&fs);
+//	xs.Write("Hello");  // "Hello" 앞뒤에 XML Tag 를 추가해서 쓴다
+
 	EncryptStream es(&fs);
 	es.Write("Hello"); 
 
 	ZipStream zs(&es);
 	zs.Write("Hello"); 
+
+	// 새로운 기능 추가가 필요하면
+	// 새로운 Decorator만 만들면 됩니다
 }
 
+// C# 언어가 위 처럼 "Decorator"패턴으로 되어 있습니다.
 
+// 저장소				Decorator
+// FileStream			EncryptStream
+// PipeStream			GZipStream
+// NetworkStream		XmlStream 
+//						HtmlStream
 
 
 
