edbd1bd0174ca3df47e983cb73cfde6e56a7274a
ecourse <smkang@codenuri.co.kr>
2021-06-08 08:29:37 +0900

    Create Singleton3.cpp

---
+++ 오전수업/DAY7/Singleton3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+// 오직 한개의 객체를 힙에 만드는 싱글톤.
+class Cursor
+{
+private:
+	Cursor() {}
+	Cursor(const Cursor&) = delete;
+	Cursor& operator=(const Cursor&) = delete;
+	
+	static Cursor* sinstance;
+public:
+	static Cursor& getInstance()
+	{
+		if (sinstance == nullptr)
+			sinstance = new Cursor;
+
+		return instance;
+	}
+};
+Cursor* Cursor::sinstance = nullptr;
+
+
+
+
+
+int main()
+{
+	Cursor& c1 = Cursor::getInstance();
+	Cursor& c2 = Cursor::getInstance();
+	std::cout << &c1 << ", " << &c2 << std::endl;
+
+}
