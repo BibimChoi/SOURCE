7b813721522288567d8c7648a963014566efeddb
ecourse <smkang@codenuri.co.kr>
2021-06-08 13:54:09 +0900

    Create Singleton4.cpp

---
+++ 오후수업/DAY7/Singleton4.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <mutex>
+
+class Cursor
+{
+private:
+	Cursor() {}
+
+	Cursor(const Cursor&) = delete;
+	Cursor& operator=(const Cursor&) = delete;
+
+	static std::mutex mtx;
+	static Cursor* sinstance;
+public:
+	static Cursor& getInstance()
+	{
+		mtx.lock();
+
+		if (sinstance == nullptr)
+			sinstance = new Cursor;
+
+		mtx.unlock();
+		return *sinstance;
+	}
+};
+Cursor* Cursor::sinstance = nullptr;
+std::mutex  Cursor::mtx;
+
+
+
+
+
+
+int main()
+{
+	Cursor& c1 = Cursor::getInstance();
+	Cursor& c2 = Cursor::getInstance();
+
+	std::cout << &c1 << ", " << &c2 << std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+
