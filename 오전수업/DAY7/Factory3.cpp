f1ac02485cf8e0b7bf0fc9308cd9cdcfb2cc979a
ecourse <smkang@codenuri.co.kr>
2021-06-08 10:37:21 +0900

    Create Factory3.cpp

---
+++ 오전수업/DAY7/Factory3.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <map>
+#include "helper.h"
+
+class Shape
+{
+public:
+	virtual void Draw() = 0;
+	virtual ~Shape() {}
+};
+
+class ShapeFactory
+{
+	MAKE_SINGLENTON(ShapeFactory)
+
+	typedef Shape*(*CREATE_FUNCTION)();
+
+	std::map<int, CREATE_FUNCTION> create_map;
+public:
+	void Register(int key, CREATE_FUNCTION f)
+	{
+		create_map[key] = f;
+	}
+
+	Shape* Create(int type)
+	{
+		Shape* p = nullptr;
+
+		if (create_map[type] != nullptr)
+		{
+			p = create_map[type]();
+			return p;
+		}
+	}
+};
+
+// 공장에 제품(도형)을 자동으로 등록하는 클래스
+class RegisterShape
+{
+public:
+	RegisterShape(int key, Shape*(*f)() )
+	{
+		ShapeFactory& factory = ShapeFactory::getInstance();
+		factory.Register(key, f);
+	}
+};
+// 아래 한줄을 생각해 보세요
+// 핵심 : main 함수 보다 전역변수 생성자가 먼저 호출됩니다.
+//RegisterShape rs(1, &Rect::Create);
+
+class Rect : public Shape
+{
+public:
+	void Draw() override { std::cout << "Draw Rect" << std::endl; }
+
+	static Shape* Create() { return new Rect; }
+	
+	// static 멤버 데이타의 특징을 잘 생각해 보세요
+	// 1. 모든 객체가 공유
+	// 2. 객체를 만들지 않아도 프로그램 시작시 메모리에 놓인다.(생성자가 호출된다는 것)
+	static RegisterShape rs;
+};
+RegisterShape Rect::rs(1, &Rect::Create);
+
+
+
+class Circle : public Shape
+{
+public:
+	void Draw() override { std::cout << "Draw Circle" << std::endl; }
+
+	static Shape* Create() { return new Circle; }
+
+	static RegisterShape rs;
+};
+RegisterShape Circle::rs(2, &Circle::Create);
+
+
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	ShapeFactory& factory = ShapeFactory::getInstance();
+
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd >= 1 && cmd <= 7) 
+		{
+			Shape* p = factory.Create(cmd);
+
+			if (p != nullptr)
+				v.push_back(p);
+
+		}
+		else if (cmd == 9)
+		{
+			for (auto p : v)
+				p->Draw();
+		}
+	}
+}
+
+
