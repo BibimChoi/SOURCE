5f23a07882a7d8f80ad342086d7be6deabc06b56
ecourse <smkang@codenuri.co.kr>
2021-06-08 16:34:09 +0900

    Update static4.cpp

--- 오후수업/DAY7/static4.cpp
+++ 오후수업/DAY7/static4.cpp
@@ -1,16 +1,39 @@
 // static4.cpp
 
 // 아래 코드에서 에러가 나오는 부분을 찾으세요
+/*
 class A
 {
 	int data;
 public:
 	void foo()
 	{
-		*data = 10;
+		*data = 10; // error
 	}
 };
 int main()
 {
 	A a;
 }
+*/
+template<typename T>
+class A
+{
+	T data;
+public:
+	void foo()
+	{
+		*data = 10; // error
+	}
+};
+int main()
+{
+	A<int> a;
+}
+
+
+
+
+
+
+
