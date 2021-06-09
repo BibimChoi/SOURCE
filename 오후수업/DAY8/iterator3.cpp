8e5090b13cb5aa947dac3b9ceee98f9f8792ad18
ecourse <smkang@codenuri.co.kr>
2021-06-09 15:40:50 +0900

    Create iterator3.cpp

---
+++ 오후수업/DAY8/iterator3.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+template<typename T>
+struct Node
+{
+	T  data;
+	Node* next;
+
+	Node(const T& d, Node* n) : data(d), next(n) {}
+};
+
+
+
+
+// STL 버전의 반복자
+
+// 1. 함수의 이름을 진짜 포인터의 연산과 동일하게
+
+template<typename T> class slist_iterator 
+{
+	Node<T>* current;
+public:
+	slist_iterator(Node<T>* p = nullptr) : current(p) {}
+
+	inline slist_iterator& operator++()
+	{
+		current = current->next;
+		return *this; // ++연산자 규칙(자신의 참조 반환)
+	}
+
+	inline T& operator*()
+	{
+		return current->data;
+	}
+};
+
+
+template<typename T>
+class slist 
+{
+	Node<T>* head = nullptr;
+public:
+	void push_front(const T& a) { head = new Node<T>(a, head); }
+	T& front() { return head->data; }
+
+	slist_iterator<T> begin()
+	{
+		return slist_iterator<T>(head); // new를 사용하지 않고. 일반 스택객체로 반환
+	}
+};
+
+int main()
+{
+	slist<int> s;
+	s.push_front(10);
+	s.push_front(20);
+	s.push_front(30);
+	s.push_front(40);
+
+	slist_iterator<int> p = s.begin();
+
+	std::cout << *p << std::endl;
+
+	++p;
+	std::cout << *p << std::endl;
+
+	++p;
+	std::cout << *p << std::endl;
+
+}
+
+
+
