a572044f22804fa832d48234cc98ef957ffb09b8
ecourse <smkang@codenuri.co.kr>
2021-06-09 16:23:15 +0900

    Create visitor2-1.cpp

---
+++ 오후수업/DAY8/visitor2-1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <list>
+#include <vector>
+
+
+// 방문자의 인터페이스
+template<typename T> struct IVisitor
+{
+	virtual void Visit(T& e) = 0;
+	virtual ~IVisitor() {}
+};
+
+template<typename T> class TwiceVisitor : public IVisitor<T>
+{
+public:
+	void Visit(T& e) { e = e * 2; }
+};
+
+template<typename T> class ShowVisitor : public IVisitor<T>
+{
+public:
+	void Visit(T& e) { std::cout << e << ", "; }
+};
+
+// 컨테이너는 방문자를 받아들여야 한다.
+template<typename T> struct IAcceptor
+{
+	virtual void Accept(IVisitor<T>*) = 0;
+	virtual ~IAcceptor() {}
+};
+
+template<typename T> class MyList : public std::list<T>, public IAcceptor<T>
+{
+public:
+	using std::list<T>::list;  
+
+	void Accept(IVisitor<T>* visitor)
+	{
+		for (auto& e : *this)
+			visitor->Visit(e);
+	}
+};
+int main()
+{
+	MyList<int> s = { 1,2,3,4,5,6,7,8,9,10 };
+
+	TwiceVisitor<int> tv;
+	s.Accept(&tv);
+
+	ShowVisitor<int> sv; // 모든 요소를 화면 출력하는 방문자
+	s.Accept(&sv);
+
+	// 방문자는 list 뿐아니라 모든 컨테이너를 방문해야 합니다.
+//	MyVector<int> v = { 1,2,3,4 };
+//	v.Accept(&tv);
+}
+
+
+
+
+
