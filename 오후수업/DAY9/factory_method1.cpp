3fe9717549c8e1aad287bb4f2b5031ae30d66569
ecourse <smkang@codenuri.co.kr>
2021-06-10 14:42:18 +0900

    Create factory_method1.cpp

---
+++ 오후수업/DAY9/factory_method1.cpp
@@ -0,0 +1,81 @@
+// 3_추상팩토리 - 147
+#include <iostream>
+
+// 모든 종류의 컨트롤은 공통의 기반 클래스 필요
+struct IButton
+{
+	virtual void Draw() = 0;
+	virtual ~IButton() {}
+};
+struct IEdit
+{
+	virtual void Draw() = 0;
+	virtual ~IEdit() {}
+};
+
+
+struct WinButton : public IButton
+{
+	void Draw() { std::cout << "Draw WinButton" << std::endl; }
+};
+struct WinEdit : public IEdit
+{
+	void Draw() { std::cout << "Draw WinEdit" << std::endl; }
+};
+
+struct OSXButton : public IButton
+{
+	void Draw() { std::cout << "Draw OSXButton" << std::endl; }
+};
+struct OSXEdit : public IEdit
+{
+	void Draw() { cout << "Draw OSXButton" << endl; }
+};
+
+//--------------------------------
+
+// 스타일 옵션에 상관없이 항상 Windows 모양으로 나타나는 Dialog
+class WinDialog
+{
+public:
+	void init()
+	{
+		WinButton* btn = new WinButton;
+		WinEdit*   edit = new WinEdit;
+
+//		btn->Move(); 
+//		edit->Move();
+
+		btn->Draw();
+		edit->Draw();
+	}
+};
+class OSXDialog
+{
+public:
+	void init()
+	{
+		OSXButton* btn = new OSXButton;
+		OSXEdit*   edit = new OSXEdit;
+
+		//		btn->Move(); 
+		//		edit->Move();
+
+		btn->Draw();
+		edit->Draw();
+	}
+};
+
+
+int main(int argc, char** argv)
+{
+
+}
+
+
+
+
+
+
+
+
